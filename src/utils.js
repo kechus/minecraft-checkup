0fdfd2f48849cf0609f1e1fc9fed291c430437cc
kechus <kechus4556@gmail.com>
2022-01-12 22:18:29 -0600

    Deployment issues

--- src/utils.js
+++ src/utils.js
@@ -8,11 +8,11 @@ const ALL_ADVANCEMENTS = [
 ]
 
 const IMAGE_PATHS = {
-  BIOMES: process.env.PUBLIC_URL + '/images/biomes.png',
-  ENTITIES: process.env.PUBLIC_URL + '/images/entities.png',
-  ITEMS: process.env.PUBLIC_URL + '/images/items.png',
-  INV: process.env.PUBLIC_URL + '/images/inv.png',
-  FRAME: process.env.PUBLIC_URL + '/images/trophy.png',
+  BIOMES: './images/biomes.png',
+  ENTITIES: './images/entities.png',
+  ITEMS: './images/items.png',
+  INV: './images/inv.png',
+  FRAME: './images/trophy.png',
 }
 
 class Advancement {
@@ -26,17 +26,17 @@ class Advancement {
 
 const ADVANCEMENTS_NAMES = {
   "minecraft:husbandry/balanced_diet": new Advancement(
-    "A balanced diet", '-768px -3168px', IMAGE_PATHS.ITEMS, '/jsons/balanced.json'),
+    "A balanced diet", '-768px -3168px', IMAGE_PATHS.ITEMS, './jsons/balanced.json'),
   "minecraft:adventure/kill_all_mobs": new Advancement(
-    'Monsters Hunted', '-576px -3616px', IMAGE_PATHS.ENTITIES, '/jsons/hunted.json'),
+    'Monsters Hunted', '-576px -3616px', IMAGE_PATHS.ENTITIES, './jsons/hunted.json'),
   "minecraft:adventure/adventuring_time": new Advancement(
-    'Adventuring Time', '-96px -2496px', IMAGE_PATHS.BIOMES, '/jsons/adventuring.json'),
+    'Adventuring Time', '-96px -2496px', IMAGE_PATHS.BIOMES, './jsons/adventuring.json'),
   "minecraft:husbandry/bred_all_animals": new Advancement(
-    'Two By Two', '-160px -3136px', IMAGE_PATHS.ENTITIES, '/jsons/twoby.json'),
+    'Two By Two', '-160px -3136px', IMAGE_PATHS.ENTITIES, './jsons/twoby.json'),
   "minecraft:husbandry/complete_catalogue": new Advancement(
-    'A complete Catalogue', '-896px -1664px', IMAGE_PATHS.ENTITIES, '/jsons/complete.json'),
+    'A complete Catalogue', '-896px -1664px', IMAGE_PATHS.ENTITIES, './jsons/complete.json'),
   "minecraft:nether/explore_nether": new Advancement(
-    'Hot Tourist Destination', '-416px -128px', IMAGE_PATHS.BIOMES, '/jsons/hot.json'),
+    'Hot Tourist Destination', '-416px -128px', IMAGE_PATHS.BIOMES, './jsons/hot.json'),
 }
 
 export { ADVANCEMENTS_NAMES, ALL_ADVANCEMENTS, IMAGE_PATHS }
